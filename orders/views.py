87a1266a0e8998ba6009213d89cb8bd185eed973
Mauya <smkadungure@live.co.uk>
2017-12-10 23:33:49 +0000

    modified orderstemplates folder

--- orders/views.py
+++ orders/views.py
@@ -28,4 +28,4 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart,
-                                                        'form': form})
+                                                         'form': form})
