9f79191384b00a734561c0592a2379cf23ea647e
Mauya <smkadungure@live.co.uk>
2017-12-12 20:15:34 +0000

    modified the names of htmls in shop

--- shop/views.py
+++ shop/views.py
@@ -13,7 +13,7 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    return render(request, 'shop/product/list.html', {'category': category,
+    return render(request, 'shop/product_list.html', {'category': category,
                                                       'categories': categories,
                                                       'products': products})
 
@@ -22,6 +22,9 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     cart_product_form = CartAddProductForm()
     return render(request,
-                  'shop/product/detail.html',
+                  'shop/product_detail.html',
                   {'product': product,
                    'cart_product_form': cart_product_form})
+
+def shop (request):
+    return render(request, 'shop/shop.html')
