a1a60ac1ef04b05a88adaf09f1aa4d230a2d0030
Mauya <smkadungure@live.co.uk>
2017-12-15 03:30:55 +0000

    added changes to bookingForm migrations

--- booking/urls.py
+++ booking/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 # Booking form urls
 urlpatterns = [
-    url(r'^booking/$', views.booking_create, name='booking_create'),
+    url(r'^create/$', views.booking_create, name='booking_create'),
     url(r'^(?P<pk>\d+)/$', views.booking_detail, name='booking_detail'),
     url(r'^(?P<pk>\d+)/edit/$', views.booking_edit, name='booking_edit'),
     url(r'^$', views.booking_list, name='booking_list'),
