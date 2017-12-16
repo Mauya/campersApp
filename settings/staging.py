2fc84ebe894095721e344b4eebace601ee9b8dd7
Mauya <smkadungure@live.co.uk>
2017-12-16 02:15:02 +0000

    setting the staging files in heroku

--- settings/staging.py
+++ settings/staging.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -7,6 +8,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': dj_database_url.config('CLEARDB_DATABASE_URL'),
     }
 }
 
